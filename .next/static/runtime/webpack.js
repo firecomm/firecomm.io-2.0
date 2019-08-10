8d1941fd3364ecbbdc3f88ee1fce4bc87162a883
Ian Geckeler <ikcgeckeler@gmail.com>
2019-08-10 11:07:25 -0400

    added .next to .gitignore

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d46a438dbffcba4e60bf";
+/******/ 	var hotCurrentHash = "f2a08f910cdefc596e5a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
