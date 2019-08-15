ff967a7968036bfc252642b1ad1b51314f43bca0
Ian Geckeler <ikcgeckeler@gmail.com>
2019-08-14 22:24:48 -0400

    added error boundary to button components

--- components/ButtonBar.jsx
+++ components/ButtonBar.jsx
@@ -39,34 +39,35 @@ const ButtonBarStyles = styled.div`
   }
 `;
 const ButtonBar = props => {
-  console.log(props);
-  const index = props.sectionIndeces[props.subsection];
-
-  console.log(index);
-  return (
-    <ButtonBarStyles>
-      {index !== 0 ? (
-        <a href={props.linksArray[index - 1].link}>
-          <button>
-            <i class="fa fa-arrow-left" />
-            {props.linksArray[index - 1].title.toUpperCase()}
-          </button>
-        </a>
-      ) : (
-        <div />
-      )}
-      {index !== props.linksArray.length - 1 ? (
-        <a href={props.linksArray[index + 1].link}>
-          <button>
-            {props.linksArray[index + 1].title.toUpperCase()}{" "}
-            <i class="fa fa-arrow-right" />
-          </button>
-        </a>
-      ) : (
-        <div />
-      )}
-    </ButtonBarStyles>
-  );
+  try {
+    const index = props.sectionIndeces[props.subsection];
+    return (
+      <ButtonBarStyles>
+        {index !== 0 ? (
+          <a href={props.linksArray[index - 1].link}>
+            <button>
+              <i class="fa fa-arrow-left" />
+              {props.linksArray[index - 1].title.toUpperCase()}
+            </button>
+          </a>
+        ) : (
+          <div />
+        )}
+        {index !== props.linksArray.length - 1 ? (
+          <a href={props.linksArray[index + 1].link}>
+            <button>
+              {props.linksArray[index + 1].title.toUpperCase()}{" "}
+              <i class="fa fa-arrow-right" />
+            </button>
+          </a>
+        ) : (
+          <div />
+        )}
+      </ButtonBarStyles>
+    );
+  } catch {
+    return null;
+  }
 };
 
 export default ButtonBar;
--- pages/docs/gettingstarted/concepts.jsx
+++ pages/docs/gettingstarted/concepts.jsx
@@ -3,7 +3,7 @@ import DocsContent from "../../../components/DocsContent";
 import content from "../../../static/docs/GettingStarted/Concepts.md";
 
 const Concepts = props => {
-  return <DocsContent content={content} />;
+  return <DocsContent subsection={"Concepts"} content={content} />;
 };
 
 export default Concepts;
