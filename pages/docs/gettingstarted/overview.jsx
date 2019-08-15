c37bd94f0b8a8034d552b5fff1759997e9d80740
Ian Geckeler <ikcgeckeler@gmail.com>
2019-08-15 11:17:30 -0400

    combined overview and concepts in getting started section

--- pages/docs/gettingstarted/overview.jsx
+++
@@ -1,9 +0,0 @@
-import DocsContent from "../../../components/DocsContent";
-
-import content from "../../../static/docs/GettingStarted/Overview.md";
-
-const Overview = props => {
-  return <DocsContent subsection={"Overview"} content={content} />;
-};
-
-export default Overview;
